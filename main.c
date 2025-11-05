01002afae9c9880996520c7c009ff9daaa18de81
irongirl101 <aditi061806@gmail.com>
2025-11-05 19:00:48 +0530

    Dependencies work, add an edge works, adding dependencies work

--- main.c
+++ main.c
@@ -6,16 +6,18 @@
 #define MAX_VERTICES 100
 #define MAX_NAME_LEN 50
 
-
-struct queue {
-    int items[SIZE];
-    int front;
-    int rear;
+// queue structure for bfs 
+struct queue{
+    int items[SIZE]; // max number of indices
+    int front; 
+    int rear; 
 };
 
-struct queue* createQueue() {
-    struct queue* q = malloc(sizeof(struct queue));
-    q->front = q->rear = -1;
+struct queue* createQueue(){
+    struct queue* q = (struct queue*)malloc(sizeof(struct queue)); 
+    // init front and rear to -1 
+    q->front = -1; 
+    q->rear = -1; 
     return q;
 }
 
@@ -23,35 +25,44 @@ int isEmpty(struct queue* q) {
     return q->rear == -1;
 }
 
-void enqueue(struct queue* q, int value) {
-    if (q->rear == SIZE - 1) return;
-    if (q->front == -1) q->front = 0;
-    q->rear++;
-    q->items[q->rear] = value;
+void enqueue(struct queue* q, int value){
+    if(q->rear++==SIZE){
+        printf("Queue at max size"); 
+        return; 
+    }
+    if(q->front == -1){
+        q->front=0; 
+    }
+    q->items[q->rear] = value; 
 }
 
-int dequeue(struct queue* q) {
-    if (isEmpty(q)) return -1;
-    int item = q->items[q->front];
-    q->front++;
-    if (q->front > q->rear)
-        q->front = q->rear = -1;
-    return item;
+int dequeue(struct queue* q){
+    if(q->front==-1){
+        printf("Empty"); 
+        return -1; 
+    }
+    int item = q->items[q->front]; 
+    q->front++; 
+    if(q->front > q->rear){
+        q->front = q->rear = -1; 
+    }
+    return item; 
 }
 
+// adjacency list 
+struct Node{
+    int vertex; 
+    struct Node* next; 
+}; 
 
-struct Node {
-    int vertexIndex;
-    struct Node* next;
-};
+struct adjlist{
+    struct Node* head; 
+}; 
 
-struct AdjList {
-    struct Node* head;
-};
-
+// graph 
 struct Graph {
     int numVertices;
-    struct AdjList* array;
+    struct adjlist* array;
     int* visited;
     char vertexNames[MAX_VERTICES][MAX_NAME_LEN];
 };
@@ -59,7 +70,7 @@ struct Graph {
 struct Graph* createGraph(int vertices) {
     struct Graph* graph = malloc(sizeof(struct Graph));
     graph->numVertices = 0;  
-    graph->array = malloc(vertices * sizeof(struct AdjList));
+    graph->array = malloc(vertices * sizeof(struct adjlist));
     graph->visited = malloc(vertices * sizeof(int));
 
     for (int i = 0; i < vertices; i++) {
@@ -83,7 +94,7 @@ int getIndex(struct Graph* graph, const char* name) {
 
 struct Node* createNode(int vertexIndex) {
     struct Node* newNode = malloc(sizeof(struct Node));
-    newNode->vertexIndex = vertexIndex;
+    newNode->vertex = vertexIndex;
     newNode->next = NULL;
     return newNode;
 }
@@ -113,7 +124,7 @@ void bfs(struct Graph* graph, const char* startName) {
 
         struct Node* temp = graph->array[current].head;
         while (temp) {
-            int adj = temp->vertexIndex;
+            int adj = temp->vertex;
             if (graph->visited[adj] == 0) {
                 graph->visited[adj] = 1;
                 enqueue(q, adj);
@@ -129,7 +140,7 @@ void printGraph(struct Graph* graph) {
         struct Node* temp = graph->array[v].head;
         printf("\nAdjacency list of %s\n head", graph->vertexNames[v]);
         while (temp) {
-            printf(" -> %s", graph->vertexNames[temp->vertexIndex]);
+            printf(" -> %s", graph->vertexNames[temp->vertex]);
             temp = temp->next;
         }
         printf("\n");
@@ -139,21 +150,37 @@ void printGraph(struct Graph* graph) {
 
 int main() {
     struct Graph* graph = createGraph(MAX_VERTICES);
+    int tasks; 
+    printf("Enter number of tasks\n");
+    scanf("%d",&tasks); 
 
+    for(int i = 0; i<tasks;i++){
+        char task[MAX_NAME_LEN]; 
+        char dependency;  
+        printf("Enter task\n"); 
+        scanf("%s",task); 
+        printf("Do you have a dependency on this task?(y/n)\n");
+        scanf(" %c", &dependency); 
+        if(dependency=='y'){
+            int no_tasks; 
+            printf("number of tasks %s is dependent on?\n", task); 
+            scanf("%d", &no_tasks); 
+            for(int j = 0; j < no_tasks;j++){
+                char task_dependent[MAX_NAME_LEN]; 
+                printf("Enter task dependent"); 
+                scanf("%s", task_dependent); 
+                addEdge(graph, task_dependent,task); 
+            }
+        }
+        else{
+            continue; 
+        }
+    }
     
-    addEdge(graph, "Task 1", "Task 2");
-    addEdge(graph, "Task 1", "Task 3");
-    addEdge(graph, "Task 1", "Task 4");
-    addEdge(graph, "Task 2", "Task 5");
-
-    
-    addEdge(graph, "Task 6", "Task 7");
-    addEdge(graph, "Task 6", "Task 8");
-    addEdge(graph, "Task 8", "Task 9");
 
     printGraph(graph);
 
-    printf(" BFS traversal for disconnected graph \n");
+    
     for (int i = 0; i < graph->numVertices; i++) {
         if (graph->visited[i] == 0) {
             bfs(graph, graph->vertexNames[i]);
